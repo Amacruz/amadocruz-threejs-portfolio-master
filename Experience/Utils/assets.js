10d1d7cd0a156a12cbf29ccf3368c25faad8587b
amacruz <cruz.amado@gmail.com>
2022-10-06 21:42:29 -0400

    Initial

---
+++ Experience/Utils/assets.js
@@ -0,0 +1,18 @@
+export default [
+    {
+        name: "room",
+        type: "glbModel",
+        path: "/models/Finale Version 21.glb",
+    },
+    {
+        name: "screen",
+        type: "videoTexture",
+        path: "/textures/kda.mp4",
+    },
+    {
+        name: "tv",
+        type: "videoTexture",
+        path: "/textures/jd.mp4",
+    },
+    
+];
